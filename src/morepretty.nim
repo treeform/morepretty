1c7476e118f1d4bb45c51adb70151c49fe588cba
Ryan Oldenburg <guzba8@gmail.com>
2020-03-09 14:29:45 -0500

    grab imports across multiple lines, put file comments above imports

--- src/morepretty.nim
+++ src/morepretty.nim
@@ -1,46 +1,75 @@
 import algorithm, os, osproc, sequtils, sets, strutils
 
+proc formatBlankLines(input: seq[string]): seq[string] =
+  ## Remove \r and excess blank lines.
+  var
+    blankLines = 0
+
+  for i, line in input:
+    let line = line.replace("\r", "")
+    if line.strip() == "":
+      if blankLines > 0 or i == 0:
+        continue
+      inc blankLines
+    else:
+      blankLines = 0
+
+    result.add(line)
+
 proc processFile(filePath: string) =
   ## More-pretty a file.
   var
     importNextLineToo = false ## Should import next line?
     imports: seq[string]      ## List of imports in the block.
-    blankLines = 0            ## How many blank lines above?
+    input: seq[string]        ## Initial lines for a step.
+    next: seq[string]         ## Set of modified lines passed to next step.
     output: seq[string]       ## Building output lines in this file.
 
-  proc addLine(line: string) =
+  proc importsLine(line: string) =
     for lib in line.split(","):
       let lib = lib.strip()
       if lib.len > 0:
         imports.add(lib)
 
-  for line in readFile(filePath).split("\n"):
-    let line = line.replace("\r", "")
+  input = readFile(filePath).split("\n")
+
+  # Find all imports at the top of the file (possibly across multiple lines)
+  # Add all not-import lines to next
+  for line in input:
     if importNextLineToo:
-      addLine(line)
+      importsLine(line)
       importNextLineToo = false
       if line.endsWith(","):
         importNextLineToo = true
     elif line.startsWith("import"):
-      addLine(line[6..^1])
+      importsLine(line[6..^1])
       if line.endsWith(","):
         importNextLineToo = true
     else:
-      if imports.len > 0:
-        ## End of import block.
-        imports = toSeq(toHashSet(imports)) ## Remove duplicate imports
-        imports.sort()
-        output.add "import " & imports.join(", ")
-        imports.setLen(0)
+      next.add(line)
 
-      if line.strip() == "":
-        if blankLines > 0:
-          continue
-        inc blankLines
-      else:
-        blankLines = 0
+  # Holds the entire file with "import " lines stripped out
+  next = formatBlankLines(next)
 
+  for line in next:
+    # File comments before imports
+    if line.startsWith("#"):
       output.add(line)
+      continue
+
+    # Add imports back after file comments
+    if imports.len > 0:
+      imports = toSeq(toHashSet(imports)) # Remove duplicate imports
+      imports.sort()
+      # Add excess blank lines that are removed later
+      output.add("")
+      output.add("import " & imports.join(", "))
+      output.add("")
+      imports.setLen(0)
+
+    output.add(line)
+
+  output = formatBlankLines(output)
 
   writeFile(filePath, output.join("\n").strip(leading = false) & "\n")
   discard execCmdEx("nimpretty " & filePath)
